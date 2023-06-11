a3af1050fdfc882c02f428d8aa299fafc7c7aea8
Alex Mcneilly <115044628+alex-mcneilly@users.noreply.github.com>
2023-06-10 21:53:17 -0300

    Update prob3.py

--- prob3.py
+++ prob3.py
@@ -1,13 +1,13 @@
 # Alex Mcneilly
 
-# 
-# 
+# Largest Prime Factor
+# What is the largest prime factor of the number 600851475143?
 
 # 
 
-def function_name(n):
+def largest_prime(n):
   return 0
 
-my_sum = function_name(10)
+my_sum = largest_prime(10)
 
 print(my_sum)
